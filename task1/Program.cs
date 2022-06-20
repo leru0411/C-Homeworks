eb230517a8b7b49b89b7f55c2ec7f3eeebbae14f
Valeriya <leru0411@mail.ru>
2022-06-20 22:20:50 +0300

    added homework to repository

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.Write($"Число a = {a} больше числа b = {b}");
+}
+if (a < b)
+{
+    Console.Write($"Число a = {a} меньше числа b = {b}");
+}
+if (a == b)
+{
+    Console.Write($"Число a = {a} равно числу b = {b}");
+}
---
+++ task4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число больше нуля: ");
+int num = int.Parse(Console.ReadLine());
+int count = 2;
+Console.Write("1"+" ");
+while (count <= num)
+{
+    Console.Write(count+" ");
+    count += 2;
+}
